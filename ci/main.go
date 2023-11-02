b2bfc8f9c8f249e63bf66b5ce2cdb5cae6d8bdd2
arambrosius <aambrosius@charter.net>
2023-11-02 10:46:55 -0500

    fixed project name

--- ci/main.go
+++ ci/main.go
@@ -1,90 +1,90 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    "os"
+	"context"
+	"fmt"
+	"os"
 
-    run "cloud.google.com/go/run/apiv2"
-    runpb "cloud.google.com/go/run/apiv2/runpb"
-    "dagger.io/dagger"
+	run "cloud.google.com/go/run/apiv2"
+	runpb "cloud.google.com/go/run/apiv2/runpb"
+	"dagger.io/dagger"
 )
 
-const GCR_SERVICE_URL = "projects/fantasy-app/locations/us-central1/services/fantasy-api"
-const GCR_PUBLISH_ADDRESS = "gcr.io/fantasy-app/fantasy-api"
+const GCR_SERVICE_URL = "projects/fantasy-app-403821/locations/us-central1/services/fantasy-api"
+const GCR_PUBLISH_ADDRESS = "gcr.io/fantasy-app-403821/fantasy-api"
 
 func main() {
-    // create Dagger client
-    ctx := context.Background()
-    daggerClient, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
-    if err != nil {
-        panic(err)
-    }
-    defer daggerClient.Close()
+	// create Dagger client
+	ctx := context.Background()
+	daggerClient, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
+	if err != nil {
+		panic(err)
+	}
+	defer daggerClient.Close()
 
-    // get working directory on host
-    source := daggerClient.Host().Directory(".", dagger.HostDirectoryOpts{
-        Exclude: []string{"ci", "target"},
-    })
+	// get working directory on host
+	source := daggerClient.Host().Directory(".", dagger.HostDirectoryOpts{
+		Exclude: []string{"ci", "target"},
+	})
 
-    // build application
-    rust := daggerClient.Container(dagger.ContainerOpts{Platform: "linux/amd64"}).
-        From("rust:1.73")
+	// build application
+	rust := daggerClient.Container(dagger.ContainerOpts{Platform: "linux/amd64"}).
+		From("rust:1.73")
 
-    c := rust.
-        WithDirectory("/fantasy-api", source).
-        WithWorkdir("/fantasy-api").
-        WithExec([]string{"cargo", "build", "--release"})
+	c := rust.
+		WithDirectory("/fantasy-api", source).
+		WithWorkdir("/fantasy-api").
+		WithExec([]string{"cargo", "build", "--release"})
 
-    // publish container to Google Container Registry
-    addr, err := c.Publish(ctx, GCR_PUBLISH_ADDRESS)
-    if err != nil {
-        panic(err)
-    }
+	// publish container to Google Container Registry
+	addr, err := c.Publish(ctx, GCR_PUBLISH_ADDRESS)
+	if err != nil {
+		panic(err)
+	}
 
-    // print ref
-    fmt.Println("Published at:", addr)
+	// print ref
+	fmt.Println("Published at:", addr)
 
-    // create Google Cloud Run client
-    gcrClient, err := run.NewServicesClient(ctx)
-    if err != nil {
-        panic(err)
-    }
-    defer gcrClient.Close()
+	// create Google Cloud Run client
+	gcrClient, err := run.NewServicesClient(ctx)
+	if err != nil {
+		panic(err)
+	}
+	defer gcrClient.Close()
 
-    // define service request
-    gcrRequest := &runpb.UpdateServiceRequest{
-        Service: &runpb.Service{
-            Name: GCR_SERVICE_URL,
-            Template: &runpb.RevisionTemplate{
-                Containers: []*runpb.Container{
-                    {
-                        Image: addr,
-                        Ports: []*runpb.ContainerPort{
-                            {
-                                Name:          "http1",
-                                ContainerPort: 3030,
-                            },
-                        },
-                    },
-                },
-            },
-        },
-    }
+	// define service request
+	gcrRequest := &runpb.UpdateServiceRequest{
+		Service: &runpb.Service{
+			Name: GCR_SERVICE_URL,
+			Template: &runpb.RevisionTemplate{
+				Containers: []*runpb.Container{
+					{
+						Image: addr,
+						Ports: []*runpb.ContainerPort{
+							{
+								Name:          "http1",
+								ContainerPort: 3030,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
 
-    // update service
-    gcrOperation, err := gcrClient.UpdateService(ctx, gcrRequest)
-    if err != nil {
-        panic(err)
-    }
+	// update service
+	gcrOperation, err := gcrClient.UpdateService(ctx, gcrRequest)
+	if err != nil {
+		panic(err)
+	}
 
-    // wait for service request completion
-    gcrResponse, err := gcrOperation.Wait(ctx)
-    if err != nil {
-        panic(err)
-    }
+	// wait for service request completion
+	gcrResponse, err := gcrOperation.Wait(ctx)
+	if err != nil {
+		panic(err)
+	}
 
-    // print ref
-    fmt.Println("Deployment for image", addr, "now available at", gcrResponse.Uri)
+	// print ref
+	fmt.Println("Deployment for image", addr, "now available at", gcrResponse.Uri)
 
 }
