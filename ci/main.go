de87d1feffcde3ff0c7d0859a346f16a2422adfa
arambrosius <aambrosius@charter.net>
2023-11-04 08:24:54 -0500

    use gce instead of gcr

--- ci/main.go
+++ ci/main.go
@@ -3,15 +3,17 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 
-	run "cloud.google.com/go/run/apiv2"
-	runpb "cloud.google.com/go/run/apiv2/runpb"
 	"dagger.io/dagger"
+	"google.golang.org/api/compute/v1"
 )
 
 const GCR_SERVICE_URL = "projects/fantasy-app-403821/locations/us-central1/services/fantasy-api"
 const GCR_PUBLISH_ADDRESS = "gcr.io/fantasy-app-403821/fantasy-api"
+
+// us-central1-docker.pkg.dev/fantasy-app-403821/fantasy-api
 
 func main() {
 	// create Dagger client
@@ -25,6 +27,7 @@ func main() {
 	// get working directory on host
 	source := daggerClient.Host().Directory(".", dagger.HostDirectoryOpts{
 		Exclude: []string{"ci", "target"},
+		Include: []string{"src", "Cargo.toml", "Cargo.lock"},
 	})
 
 	// build application
@@ -50,58 +53,87 @@ func main() {
 	// print ref
 	fmt.Println("Published at:", addr)
 
-	// create Google Cloud Run client
-	gcrClient, err := run.NewServicesClient(ctx)
+	computeService, err := compute.NewService(ctx)
 	if err != nil {
-		panic(err)
+		log.Fatalf("Failed to create Compute Engine service client: %v", err)
 	}
-	defer gcrClient.Close()
 
-	// define service request
-	gcrRequest := &runpb.UpdateServiceRequest{
-		Service: &runpb.Service{
-			Name:    GCR_SERVICE_URL,
-			Ingress: runpb.IngressTraffic_INGRESS_TRAFFIC_ALL,
-			Template: &runpb.RevisionTemplate{
-				MaxInstanceRequestConcurrency: 1,
-				Containers: []*runpb.Container{
-					{
-						Image: addr,
-						Ports: []*runpb.ContainerPort{
-							{
-								Name:          "http1",
-								ContainerPort: 3030,
-							},
-						},
-						Resources: &runpb.ResourceRequirements{
-							Limits: map[string]string{
-								"cpu":    "1",
-								"memory": "4Gi",
-							},
-						},
-					},
-				},
-				Scaling: &runpb.RevisionScaling{
-					MinInstanceCount: 0,
-					MaxInstanceCount: 1,
+	instanceName := "fantasy-api"
+	metadataValue := "spec:\n  containers:\n    - name: fantasy-api\n      image: " + GCR_PUBLISH_ADDRESS + "\n      stdin: false\n      tty: false,\n  restartPolicy: Always\n  port: 8080\n"
+
+	updatedInstance := &compute.Instance{
+		Name:        instanceName,
+		Description: "API to host the fantasy app",
+		MachineType: "e2-micro",
+
+		// NetworkInterfaces: []*compute.NetworkInterface{
+		// 	{
+		// 		Network: "global/networks/default",
+		// 		AccessConfigs: []*compute.AccessConfig{
+		// 			{
+		// 				Name: "External NAT",
+		// 				Type: "ONE_TO_ONE_NAT",
+		// 			},
+		// 		},
+		// 	},
+		// },
+		Metadata: &compute.Metadata{
+			Items: []*compute.MetadataItems{
+				{
+					Key:   "gce-container-declaration",
+					Value: &metadataValue,
 				},
 			},
 		},
 	}
 
-	// update service
-	gcrOperation, err := gcrClient.UpdateService(ctx, gcrRequest)
+	// define service request
+	// gcrRequest := &compute.UpdateServiceRequest{
+	// 	Service: &runpb.Service{
+	// 		Name:    GCR_SERVICE_URL,
+	// 		Ingress: runpb.IngressTraffic_INGRESS_TRAFFIC_ALL,
+	// 		Template: &runpb.RevisionTemplate{
+	// 			MaxInstanceRequestConcurrency: 1,
+	// 			Containers: []*runpb.Container{
+	// 				{
+	// 					Image: addr,
+	// 					Ports: []*runpb.ContainerPort{
+	// 						{
+	// 							Name:          "http1",
+	// 							ContainerPort: 8080,
+	// 						},
+	// 					},
+	// 					Resources: &runpb.ResourceRequirements{
+	// 						Limits: map[string]string{
+	// 							"cpu":    "1",
+	// 							"memory": "512Mi",
+	// 						},
+	// 					},
+	// 				},
+	// 			},
+	// 			Scaling: &runpb.RevisionScaling{
+	// 				MinInstanceCount: 0,
+	// 				MaxInstanceCount: 1,
+	// 			},
+	// 		},
+	// 	},
+	// }
+
+	// Update the GCE instance.
+	op, err := computeService.Instances.Update("fantasy-app-403821", "us-cental1-c", instanceName, updatedInstance).Context(ctx).Do()
 	if err != nil {
-		panic(err)
+		log.Fatalf("Failed to update GCE instance: %v", err)
 	}
 
-	// wait for service request completion
-	gcrResponse, err := gcrOperation.Wait(ctx)
-	if err != nil {
-		panic(err)
-	}
+	fmt.Printf("Instance update operation: %v\n", op)
+
+	// // wait for service request completion
+	// gcrResponse, err := gcrOperation.Wait(ctx)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
 	// print ref
-	fmt.Println("Deployment for image", addr, "now available at", gcrResponse.Uri)
+	// fmt.Println("Deployment for image", addr, "now available at", gcrResponse.Uri)
 
 }
