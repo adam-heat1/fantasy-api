3111a1fe7df1ea5e2a66fe19ceaea22a032e643c
arambrosius <aambrosius@charter.net>
2023-11-20 12:19:55 -0600

    added news to httpServer

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use actix_web::{web::scope, App, HttpServer};
 use dotenv::dotenv;
-use fantasy_api::handlers::account;
+use fantasy_api::handlers::{account, news};
 use log::info;
 
 #[actix_web::main]
@@ -9,8 +9,12 @@ async fn main() -> std::io::Result<()> {
 
     info!("Starting server on 8080");
 
-    HttpServer::new(|| App::new().service(scope("/v1/account").configure(account::configure)))
-        .bind("0.0.0.0:8080")?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(scope("/v1/account").configure(account::configure))
+            .service(scope("/v1/news").configure(news::configure))
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
 }
